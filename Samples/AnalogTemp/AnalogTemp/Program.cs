ce5c8c0a193ff326ce66098930ba653bc8464791
jixer <gsxr.signup@gmail.com>
2014-03-22 00:56:58 -0700

    analog temp sensor + sketch

---
+++ Samples/AnalogTemp/AnalogTemp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware;
+using SecretLabs.NETMF.Hardware.Netduino;
+
+namespace AnalogTemp
+{
+    public class Program
+    {
+        private static AnalogInput sensorPin = new AnalogInput(Cpu.AnalogChannel.ANALOG_0, 1, 0, -1);
+        
+        public static void Main()
+        {            
+            while (true)
+            {
+                int adcValue = sensorPin.ReadRaw();
+                double temperature = Thermister(adcValue);
+                Debug.Print("Temp value: " + temperature.ToString());
+                Thread.Sleep(500);
+            }
+        }
+
+        public static double Thermister(int RawADC)
+        {
+            double Temp;
+            Temp = System.Math.Log(((10240000000 / RawADC) - 10000));
+            Temp = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * Temp * Temp)) * Temp);
+            Temp = Temp - 273.15; // Convert Kelvin to Celcius
+            return Temp * -1;
+        }
+
+    }
+}
